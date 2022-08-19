65671e5a1e53a9eec7078d29c6874b512533e14c
Sonya Amini <soamini@microsoft.com>
2022-08-19 14:02:56 -0700

    added static content

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+export default function Card() {
+    return (
+        <div className="card">
+            {/* <img src={}  className="card--image" /> */}
+            <div>
+                {/* <img src={} className="card--star" /> */}
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
+    )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
+import airbnblogo from '../images/airbnb-logo.png'
+
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/airbnb-logo.png" className="nav--logo" alt="airbnb logo"/>
+            <img src={airbnblogo} className="nav--logo" alt="airbnb logo"/>
         </nav>
     )
 }
