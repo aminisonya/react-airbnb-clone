67911d3715963eb1322f9f320174c14956e2b1ea
Sonya Amini <soamini@microsoft.com>
2022-08-19 18:02:02 -0700

    fin airbnb clone

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 
 export default function App() {
+    const cards = data.map(x => {
+      return (
+        <Card 
+            key={x.id}
+            item={x}
+        />
+      )
+    })
+
     return (
         <div>
           <Navbar />
           <Hero />
-          <Card />
+          <section className="cards-list">
+            {cards}
+          </section>
         </div>
     )
 }
